4b6c769ae6adb0bd42de4188aead447c2095d92f
Matheus Pereira <matheper@gmail.com>
2014-04-25 02:01:12 -0300

    Bombas desenhadas.

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,6 +1,8 @@
 var canvas;
 var context;
 var keysPressed = [];
+var matrizCampos = [];
+var tamanho = 32;
 
 window.onload = function(){
     startUp();
@@ -20,18 +22,72 @@ $(document).keyup(function(e){
 function startUp(){
     canvas = document.getElementById('game_canvas');
     context = canvas.getContext('2d');
-    window.setInterval( function(){
-        gameLoop();
-    }, 15);
+    num_linhas = 10;
+    num_colunas = 20;
+    num_bombas = 20;
+    criarCampos(num_linhas, num_colunas, num_bombas);
+    canvas.setAttribute('height', num_linhas * tamanho);
+    canvas.setAttribute('width', num_colunas * tamanho);
+    window.setInterval( function(){ gameLoop();}, 15);
 }
 
 function gameLoop(){
+    drawMatriz();
+//    clearCanvas();
 }
 
-//function drawObj(context, obj){
-//    context.drawImage(obj.sprite, obj.x, obj.y);
-//}
+function drawMatriz(){
+    for(i=0; i<matrizCampos.length; i++){
+        for(j=0; j<matrizCampos[i].length; j++){
+            if(matrizCampos[i][j]){
+                drawObj(matrizCampos[i][j]);
+            }
+        }
+    }
+}
 
-function clearCanvas(canvas){
+function drawObj(obj){
+    context.drawImage(obj.sprite, obj.x, obj.y, 32, 32);
+}
+
+function clearCanvas(){
     canvas.width = canvas.width;
 }
+
+Bomba = function(linha, coluna){
+    var sprite = new Image();
+    sprite.src = "images/bomba.png";
+    return{
+        x:linha * tamanho,
+        y:coluna * tamanho,
+        sprite:sprite,
+    }
+}
+
+function criarCampos(num_linhas, num_colunas, num_bombas){
+    matrizCampos = [];
+    for(var i=0; i<num_linhas; i++) {
+        matrizCampos[i] = new Array(num_colunas);
+    }
+    
+    bombas_criadas = 0;
+    while(bombas_criadas < num_bombas) {
+        indice_linha = Math.floor(Math.random() * (num_linhas));
+        indice_coluna = Math.floor(Math.random() * (num_colunas));
+        if(!matrizCampos[indice_linha][indice_coluna]){
+            matrizCampos[indice_linha][indice_coluna] = new Bomba(indice_coluna,
+                                                                  indice_linha);
+            console.log(indice_linha, indice_coluna);
+            console.log(bombas_criadas);
+            bombas_criadas ++;
+        }
+    }
+
+    for(var i=0; i<num_linhas; i++) {
+        for(var j=0; j<num_colunas; j++){
+//            console.log(matrizCampos[i],[j]);
+        }
+    }
+
+}
+
